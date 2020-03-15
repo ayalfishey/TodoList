18c6d88482e13b940b7140d5035d689b53b4cdb6
ayalfishey <ayalfishey@gmail.comgit config --global user.email ayalfishey@gmail.comgit config --global user.email ayalfishey@gmail.com>
2020-03-15 19:42:29 +0200

    Model built

---
+++ app/src/main/java/com/ayal/todo/ToDo.java
@@ -0,0 +1,17 @@
+package com.ayal.todo;
+
+public class ToDo {
+    private String todo;
+
+    public ToDo(String todo) {
+        this.todo = todo;
+    }
+
+    public String getTodo() {
+        return todo;
+    }
+
+    public void setTodo(String todo) {
+        this.todo = todo;
+    }
+}
