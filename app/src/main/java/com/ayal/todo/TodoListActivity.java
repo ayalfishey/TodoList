59de09c170fbf112dd81934cd68504182f761c6c
ayalfishey <ayalfishey@gmail.comgit config --global user.email ayalfishey@gmail.comgit config --global user.email ayalfishey@gmail.com>
2020-03-15 20:53:28 +0200

    Added RecyclerView and Lisener for Add button

--- app/src/main/java/com/ayal/todo/TodoListActivity.java
+++ app/src/main/java/com/ayal/todo/TodoListActivity.java
@@ -1,20 +1,50 @@
 package com.ayal.todo;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TodoListActivity extends AppCompatActivity {
 
+    final int R_ID = 235878;
+    private RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_list);
-        RecyclerView recyclerView = findViewById(R.id.recycle_container);
+        Button add = bindButton((Button)findViewById(R.id.addTask));
+    }
+
+    private void setViews(){
+        recyclerView = findViewById(R.id.recycle_container);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
         TodoAdapter adapter = new TodoAdapter();
         recyclerView.setAdapter(adapter);
+
+    }
+
+    private Button bindButton (Button button){
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (TodoListActivity.this, AddTodoActivity.class);
+                startActivityForResult(intent , R_ID);
+            }
+        });
+        return button;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK){
+            setViews();
+        }
     }
 }
